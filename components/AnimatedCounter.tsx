94022855027aaca33e7bce6b5734eb056bc3521f
Muhammad <mhk8367@mavs.uta.edu>
2024-06-01 20:33:08 -0500

    fix bugs

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -7,7 +7,8 @@ const AnimatedCounter = ({ amount }: { amount: number }) => {
     <div className="w-full">
       <CountUp 
         decimals={2}
-        decimal=","
+        separator=","
+        decimal="."
         prefix="$"
         end={amount} 
       />
